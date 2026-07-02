854528a37f964c53241749c849735ebb5e498385
Prince4017 <unknown46790@gmail.com>
2026-07-02 20:14:28 +0530

    auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
-import User from '../models/User.js';
-import { generateToken } from '../utils/generateToken.js';
-import { protect } from '../middleware/auth.js';
+import User from '../User.js';
+import { generateToken } from '../generateToken.js';
+import { protect } from '../auth.js';
 
 const router = express.Router();
 
